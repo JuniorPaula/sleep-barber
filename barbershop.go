4ac0dee22295594c37da8014f1193edcb6c3ce7d
Junior Paula <luke.junnior@icloud.com>
2023-10-08 11:59:55 -0300

    Implements method to add a barber

--- barbershop.go
+++ barbershop.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"time"
+
+	"github.com/fatih/color"
+)
 
 type BarberShop struct {
 	ShopCapacity    int
@@ -10,3 +14,34 @@ type BarberShop struct {
 	ClientsChan     chan string
 	Open            bool
 }
+
+func (shop *BarberShop) addBarber(barber string) {
+	shop.NumberOfBarbers++
+
+	go func() {
+		isSleeping := false
+		color.Yellow("%s goes to the wainting room to check for clients", barber)
+
+		for {
+			// if there are no clients, the barber goes to sleep
+			if len(shop.ClientsChan) == 0 {
+				color.Yellow("There is nothing to do, so %s takes a nap.", barber)
+				isSleeping = true
+			}
+
+			client, shopOpen := <-shop.ClientsChan
+			if shopOpen {
+				if isSleeping {
+					color.Yellow("%s wakes %s up.", client, barber)
+					isSleeping = false
+				}
+				// cut hair
+				shop.cutHair(barber, client)
+			} else {
+				// shop is closed, so send the barber home and close the channel
+			}
+		}
+	}()
+}
+
+func (shop *BarberShop) cutHair(barber, client string) {}
